46bcc49eeaebd1ce16c7d9693f4989f39b9f8048
Musyonchez <musyonchez@gmail.com>
2024-04-02 23:11:28 +0300

    Add logging and test4.py script

--- test4.py
+++ test4.py
@@ -3,10 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pyttsx3
-import logging
 
-# Configure logging
-logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def fetch_content(url):
     response = requests.get(url)
@@ -14,14 +11,18 @@ def fetch_content(url):
     content_div = soup.find("div", id="article")
     return content_div.get_text() if content_div else "Content not found"
 
+
 def speak_content(text_content):
     engine = pyttsx3.init()
-    engine.setProperty("rate", 180) # Speed of speech (words per minute)
+    engine.setProperty("rate", 180)  # Speed of speech (words per minute)
     voices = engine.getProperty("voices")
-    engine.setProperty("voice", voices[11].id) # Change the index to select a different voice
-    engine.setProperty("pitch", 2.5) # Increase the pitch
+    engine.setProperty(
+        "voice", voices[11].id
+    )  # Change the index to select a different voice
+    engine.setProperty("pitch", 2.5)  # Increase the pitch
     engine.say(text_content)
     engine.runAndWait()
+
 
 # Base URL for the chapters
 base_url = "https://libread.org/libread/immortality-my-cultivation-has-no-bottleneck-64015/chapter-"
@@ -33,13 +34,7 @@ num_chapters = 3 # Adjust this number based on how many chapters you want to rea
 for chapter_number in range(2, num_chapters + 1):
     # Construct the URL for the current chapter
     url = base_url + str(chapter_number)
-    # Log the URL being processed
-    logging.info(f"Processing URL: {url}")
     # Fetch the content from the URL
     text_content = fetch_content(url)
-    # Log the completion of processing the URL
-    logging.info(f"Starting processing URL: {url}")
     # Read out the content
     speak_content(text_content)
-    # Log the completion of processing the URL
-    logging.info(f"Finished processing URL: {url}")
